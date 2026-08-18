0da585ff8cce2100ea8804e2ba39d4c413ced983
Dmitriy Ryzhenko <ggooddmitriy@gmail.com>
2026-08-18 20:33:47 +0300

    Release!

---
+++ dub.h
@@ -0,0 +1,268 @@
+#ifndef DUB_H
+#define DUB_H
+
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+  int argc;
+  char** argv;
+  int i;
+} Dub;
+
+Dub   dub_create(int argc, char** argv); // Create a Dub instance
+bool  dub_end(Dub* dub);  // Have you reached the end? (No more args to parse)
+char* dub_peek(Dub* dub); // Get the current argument. (NULL when at the end)
+void  dub_next(Dub* dub); // Move to the next argument. (dub->i++)
+
+// Functions below advance to the next argument when the match/parse succeeded.
+bool dub_match(Dub* dub, char* s);
+bool dub_parse_integer(Dub* dub, int* out);
+bool dub_parse_unsigned_integer(Dub* dub, unsigned int* out);
+bool dub_parse_port(Dub* dub, unsigned int* out);
+bool dub_parse_email(Dub* dub, char** out);
+bool dub_parse_string(Dub* dub, char** out);
+
+#ifdef DUB_IMPLEMENTATION
+
+///* Internal */
+
+static int dub_internal_ascii_digit_to_int(char c) {
+  return c - 48;
+}
+
+static bool dub_internal_is_integer(char* s, int* out) {
+  // Pointer to string is NULL
+  if (s == NULL) return false;
+  // The string content is 0 length
+  if (*s == '\0') return false;
+
+  char* cursor = s;
+  int x = 0;
+  int sign = 1;
+  bool some_digit_met = false;
+  bool error_during_parsing = false;
+  bool sign_met = false;
+
+  while (true) {
+    char c = *cursor;
+    if ('0' <= c && c <= '9') {
+      some_digit_met = true;
+      x = x * 10;
+      x = x + dub_internal_ascii_digit_to_int(c);
+      cursor++;
+      continue;
+    }
+    // End of string
+    if (c == '\0') break;
+    if (c == '-' || c == '+') {
+      if (sign_met) {
+        error_during_parsing = true;
+        break;
+      }
+      switch (c) {
+        case '-': sign = -1; break;
+        case '+': sign = 1;  break;
+      }
+      sign_met = true;
+      cursor++;
+      continue;
+    }
+    // Unexpected char
+    error_during_parsing = true;
+    break;
+  }
+
+  if (error_during_parsing) return false;
+  if (sign_met) x = x * sign;
+  *out = x;
+  return true;
+}
+
+static bool dub_internal_is_unsigned_integer(char* s, unsigned int* out) {
+  // Pointer to string is NULL
+  if (s == NULL) return false;
+  // The string content is 0 length
+  if (*s == '\0') return false;
+
+  char* cursor = s;
+  unsigned int x = 0;
+  bool some_digit_met = false;
+  bool error_during_parsing = false;
+
+  while (true) {
+    char c = *cursor;
+    if ('0' <= c && c <= '9') {
+      some_digit_met = true;
+      x = x * 10;
+      x = x + dub_internal_ascii_digit_to_int(c);
+      cursor++;
+      continue;
+    }
+    // End of string
+    if (c == '\0') break;
+    // Unexpected char
+    error_during_parsing = true;
+    break;
+  }
+
+  if (error_during_parsing) return false;
+  *out = x;
+  return true;
+}
+
+/*
+  Email is validated positively if:
+    - contains @
+    - 1 <= username length <= 64
+    - 3 <= domain length <= 255
+*/
+bool dub_internal_is_email(char* s, char** out) {
+  // Pointer to string is NULL
+  if (s == NULL) return false;
+  // The string content is 0 length
+  if (*s == '\0') return false;
+
+  char* cursor = s;
+  bool error_during_parsing = false;
+  bool at_symbol_found = false;
+
+  int username_length = 0;
+  int domain_length = 0;
+
+  while (true) {
+    char c = *cursor;
+
+    if (c == '\0') {
+      if (domain_length < 3) error_during_parsing = true;
+      break;
+    }
+
+    if (c == '@') {
+      if (at_symbol_found) {
+        error_during_parsing = true;
+        break;
+      }
+
+      if (username_length < 1) {
+        error_during_parsing = true;
+        break;
+      }
+
+      at_symbol_found = true;
+      cursor++;
+      continue;
+    }
+
+    if (!at_symbol_found) {
+      username_length++;
+      if (username_length > 64) {
+        error_during_parsing = true;
+        break;
+      }
+      cursor++;
+      continue;
+    }
+
+    if (at_symbol_found) {
+      domain_length++;
+      if (domain_length > 255) {
+        error_during_parsing = true;
+        break;
+      }
+      cursor++;
+      continue;
+    }
+  }
+
+  if (error_during_parsing) return false;
+  *out = s;
+  return true;
+}
+
+  /* Internal *///
+
+Dub dub_create(int argc, char** argv) {
+  return (Dub){
+    .argc = argc,
+    .argv = argv,
+    .i = 1
+  };
+}
+
+bool dub_end(Dub* dub) {
+  return dub->i >= dub->argc;
+}
+
+char* dub_peek(Dub* dub) {
+  if (dub_end(dub)) return NULL;
+  return dub->argv[dub->i];
+}
+
+void dub_next(Dub* dub) {
+  if (!dub_end(dub)) dub->i++;
+}
+
+bool dub_match(Dub* dub, char* s) {
+  if (dub_end(dub)) return false;
+  bool match = strcmp(dub_peek(dub), s) == 0;
+  if (match) dub->i++;
+  return match;
+}
+
+bool dub_parse_integer(Dub* dub, int* out) {
+  if (dub_end(dub)) return false;
+
+  char* s = dub_peek(dub);
+  int i;
+  if (!dub_internal_is_integer(s, &i)) return false;
+  *out = i;
+  dub->i++;
+  return true;
+}
+
+bool dub_parse_unsigned_integer(Dub* dub, unsigned int* out) {
+  if (dub_end(dub)) return false;
+
+  char* s = dub_peek(dub);
+  unsigned int i;
+  if (!dub_internal_is_unsigned_integer(s, &i)) return false;
+  *out = i;
+  dub->i++;
+  return true;
+}
+
+bool dub_parse_port(Dub* dub, unsigned int* out) {
+  if (dub_end(dub)) return false;
+
+  char* s = dub_peek(dub);
+  unsigned int i;
+  if (!dub_internal_is_unsigned_integer(s, &i)) return false;
+  if (i < 0 || 65353 < i) return false;
+  *out = i;
+  dub->i++;
+  return true;
+}
+
+bool dub_parse_email(Dub* dub, char** out) {
+  if (dub_end(dub)) return false;
+
+  char* s = dub_peek(dub);
+  char* email;
+  if (!dub_internal_is_email(s, &email)) return false;
+  *out = email;
+  dub->i++;
+  return true;
+}
+
+bool dub_parse_string(Dub* dub, char** out) {
+  if (dub_end(dub)) return false;
+
+  char* s = dub_peek(dub);
+  *out = s;
+  dub->i++;
+  return true;
+}
+
+#endif /* DUB_IMPLEMENTATION */
+#endif /* DUB_H */
---
+++ readme.md
@@ -0,0 +1,171 @@
+# dub.h - Routing tree CLA parser
+
+This library helps to parse Command line arguments.
+
+The feature of Dub is that commands routing and commands handling are integrated. You can execute arbitrary code at any point or branch during the routing process.
+
+The library provides two types of functions:
+- Non-moving the current argument pointer:
+  ```
+  dub_create dub_end dub_peek
+  ```
+- Moving the current argument pointer:
+  ```
+  dub_next
+  dub_match
+  dub_parse_*
+  ```
+
+`dub_parse_*` and `dub_match` will move the argument pointer only when the parsing/matching is successfull.
+
+
+# API
+```c
+Dub   dub_create(int argc, char** argv); // Create a Dub instance
+bool  dub_end(Dub* dub);  // Have you reached the end? (No more args to parse)
+char* dub_peek(Dub* dub); // Get the current argument. (NULL when at the end)
+void  dub_next(Dub* dub); // Move to the next argument. (dub->i++)
+
+bool dub_match(Dub* dub, char* s);
+bool dub_parse_integer(Dub* dub, int* out);
+bool dub_parse_unsigned_integer(Dub* dub, unsigned int* out);
+bool dub_parse_port(Dub* dub, unsigned int* out);
+bool dub_parse_email(Dub* dub, char** out);
+bool dub_parse_string(Dub* dub, char** out);
+```
+
+# Usage
+
+## Python http.server & calendar larping
+
+```sh
+python -m http.server
+python -m http.server 9292
+python -m http.server 9292 --bind 127.0.0.1
+python -m calendar
+```
+
+```c
+#include <stdio.h>
+#include <time.h>
+
+#define DUB_IMPLEMENTATION
+#include "dub.h"
+
+int main(int argc, char** argv) {
+  Dub dub = dub_create(argc, argv);
+
+  char* host = "0.0.0.0";
+  unsigned int port = 8000;
+
+  if (dub_match(&dub, "-m")) {
+    if (dub_match(&dub, "http.server")) {
+      dub_parse_port(&dub, &port);
+      if (dub_match(&dub, "--bind")) {
+        if (!dub_parse_string(&dub, &host)) {
+          printf("Invalid bind option. expected `--bind 127.0.0.1`");
+          return -1;
+        }
+      }
+      printf("Serving HTTP on %s port %d (http://%s:%d/) ...\n", host, port, host, port);
+      return 0;  
+    }
+    if (dub_match(&dub, "calendar")) {
+      time_t t = time(NULL);
+      struct tm *today = localtime(&t);
+      printf("Today's Date: %04d-%02d-%02d\n", 
+            today->tm_year + 1900, 
+            today->tm_mon + 1, 
+            today->tm_mday);
+      return 0;
+    }
+    printf("No module named %s", dub_peek(&dub));
+    return -1;
+  }
+
+  if (!dub_end(&dub)) printf("Unknow command: %s\n", dub_peek(&dub));
+  return -1;
+}
+```
+
+## Hello world:
+
+```
+dub version
+dub plus 7 8
+dub help
+dub -h
+dub --help
+```
+
+```c
+#include <stdio.h>
+
+#define DUB_IMPLEMENTATION
+#include "dub.h"
+
+int main(int argc, char** argv) {
+  Dub dub = dub_create(argc, argv);
+
+  if (dub_match(&dub, "version")) {
+    printf("Dub version: 1.0.0\n");
+  }
+  else if (dub_match(&dub, "plus")) {
+    int a, b;
+    if (dub_parse_integer(&dub, &a) && dub_parse_integer(&dub, &b))
+      printf("%d + %d = %d\n", a, b, a + b);
+    else
+      printf("Bro, I need two numbers. Try `dub plus 2 2`\n");
+  }
+  else if (dub_match(&dub, "help") || dub_match(&dub, "-h") || dub_match(&dub, "--help")) {
+    printf("Help yourself!\n");
+  }
+  else {
+    printf(
+      "Usage:\n"
+      "version\n"
+      "plus <int> <int>\n"
+      "help --help -h\n"
+    );
+  }
+
+  return 0;
+}
+```
+
+## Parsing optional flags in any order
+
+```c
+#include <stdio.h>
+
+#define DUB_IMPLEMENTATION
+#include "dub.h"
+
+int main(int argc, char** argv) {
+  Dub dub = dub_create(argc, argv);
+
+  bool verbose = false;
+  bool ignore_case = false;
+  bool first_try = false;
+
+  // dub -v -f -i
+  while (!dub_end(&dub)) {
+    /**/ if (dub_match(&dub, "-v")) verbose = true;
+    else if (dub_match(&dub, "-i")) ignore_case = true;
+    else if (dub_match(&dub, "-f")) first_try = true;
+    else break;
+  }
+
+  printf("I'll do my job and:\n");
+  if (verbose) printf("\tbe verbose\n");
+  if (ignore_case) printf("\tignore case\n");
+  if (first_try) printf("\tsucceed in first try!\n");
+
+  return 0;
+}
+```
+
+# Inspiration
+
+Dub was inspired by [Roda](https://github.com/jeremyevans/roda)
+
